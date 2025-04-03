1180a7698adcc8d62e24232642313e1b5c1e4d7d
keviinnsmir <kevinquilumba026@gmail.com>
2025-04-03 16:23:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función para activar o desactivar el menú en dispositivos móviles
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active'); // Añade o quita la clase 'active' para mostrar u ocultar el menú
+}
